fca3da9946aa43b3b4b809bf7fedb3135bdcef43
mahimathankam <107746236+mahimathankam@users.noreply.github.com>
2024-03-03 19:42:05 +0530

    Delete new/movieproject directory

--- new/movieproject/movie/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from movie.models import Movie
-from django.http import HttpResponse
-admin.site.register(Movie)
--- new/movieproject/movie/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-class Movie(models.Model):
-    name=models.CharField(max_length=30)
-    desc=models.CharField(max_length=100)
-    year=models.IntegerField()
-    img=models.ImageField(upload_to='movie/img',null=True,blank=True)
-
-    def __str__(self):
-        return self.name
